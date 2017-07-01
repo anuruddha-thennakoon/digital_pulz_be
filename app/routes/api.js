2e930eaed8ce28a62b97d2e3df6d228001d0bcd7
Anuruddha Thennakoon <anuruddha.thennakoon@gmail.com>
2017-07-02 02:19:54 +0530

    file upload

--- app/routes/api.js
+++ app/routes/api.js
@@ -6,6 +6,7 @@ const userRoute = require('./user.route');
 const presRoute = require('./prescription.route');
 const itemRoute = require('./item.route');
 const stockRoute=require('./stock.route');
+const uploadRoute=require('./fileupload.route');
 
 //your routers
 router.use('/authenticate', authRoute);
@@ -14,7 +15,7 @@ router.use('/prescription', presRoute);
 router.use('/item', itemRoute);
 router.use('/drugs', itemRoute);
 router.use('/stock',stockRoute);
-
+router.use('/upload',uploadRoute);
 
 module.exports = router;
 
